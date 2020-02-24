b36ecaf54af056e319f04dfc9e8edfe00753791a
Jorge Rios <jorgearios31@gmail.com>
2020-02-24 20:03:47 -0300

    subida clase Carro

--- Evaluacion3/src/com/awakelab/evaluacion/modelo/Carro.java
+++ Evaluacion3/src/com/awakelab/evaluacion/modelo/Carro.java
@@ -1,5 +1,41 @@
 package com.awakelab.evaluacion.modelo;
 
 public class Carro {
+	private byte cantidadOcupantes;
+	private String fechaIngreso;
+	private byte ubicacionX;
+	private byte ubicacionY;
+	
+	public byte getCantidadOcupantes() {
+		return cantidadOcupantes;
+	}
+	public void setCantidadOcupantes(byte cantidadOcupantes) {
+		this.cantidadOcupantes = cantidadOcupantes;
+	}
+	public String getFechaIngreso() {
+		return fechaIngreso;
+	}
+	public void setFechaIngreso(String fechaIngreso) {
+		this.fechaIngreso = fechaIngreso;
+	}
+	public byte getUbicacionX() {
+		return ubicacionX;
+	}
+	public void setUbicacionX(byte ubicacionX) {
+		this.ubicacionX = ubicacionX;
+	}
+	public byte getUbicacionY() {
+		return ubicacionY;
+	}
+	public void setUbicacionY(byte ubicacionY) {
+		this.ubicacionY = ubicacionY;
+	}
+	
+	@Override
+	public String toString() {
+		return "Carro [cantidadOcupantes=" + cantidadOcupantes + ", fechaIngreso=" + fechaIngreso + ", ubicacionX="
+				+ ubicacionX + ", ubicacionY=" + ubicacionY + "]";
+	}
+	
 	
 }
